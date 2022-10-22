2f66ff066240518a9c493c1db554bee2ef07ddb6
Thierry-Pascal <thierryfleurant18@gmail.com>
2022-10-22 01:55:19 -0400

    Added Part II

---
+++ AddCommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddCommentCell.swift
+//  InstagramAssignment
+//
+//  Created by Thierry-Pascal Fleurant on 10/22/22.
+//
+
+import UIKit
+
+class AddCommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
